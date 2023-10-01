4b6ff9d879c94308047abc65cc126cdf88481000
Anderson Freitas <andersonfreitas21@gmail.com>
2023-10-01 12:10:15 -0300

    first commit

---
+++ src/main/java/br/com/dev/movies/service/movie/MovieService.java
@@ -0,0 +1,22 @@
+package br.com.dev.movies.service.movie;
+
+import br.com.dev.movies.model.movie.Movie;
+import br.com.dev.movies.repository.movie.MovieRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class MovieService {
+    @Autowired
+    private MovieRepository repository;
+
+    public List<Movie> findAllMovies() {
+        return repository.findAll();
+    }
+    public Optional<Movie> findMovieByImdbId(String imdbId) {
+        return repository.findMovieByImdbId(imdbId);
+    }
+}
