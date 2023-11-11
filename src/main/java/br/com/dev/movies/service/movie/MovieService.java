16957b192fedb99968944bc90080abe2f6563418
Anderson Freitas <andersonfreitas21@gmail.com>
2023-11-11 17:10:18 -0300

    feat: Adding MovieService test

--- src/main/java/br/com/dev/movies/service/movie/MovieService.java
+++ src/main/java/br/com/dev/movies/service/movie/MovieService.java
@@ -2,16 +2,16 @@ package br.com.dev.movies.service.movie;
 
 import br.com.dev.movies.model.movie.Movie;
 import br.com.dev.movies.repository.movie.MovieRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
 @Service
+@RequiredArgsConstructor
 public class MovieService {
-    @Autowired
-    private MovieRepository repository;
+    private final MovieRepository repository;
 
     public List<Movie> findAllMovies() {
         return repository.findAll();
